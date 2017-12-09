d550bc7b95c26cfc3f15e21dfac9acfa77eb69f4
Andre Schmidt <andre.osku.schmidt@gmail.com>
2017-12-09 14:16:15 +0100

    refactor

--- webextension/background.js
+++ webextension/background.js
@@ -1,3 +1,14 @@
+const events = [
+	{ name: "BeforeRequest",     specs: ["requestBody"] },
+	{ name: "BeforeSendHeaders", specs: ["requestHeaders"] },
+	{ name: "SendHeaders",       specs: ["requestHeaders"] },
+	{ name: "HeadersReceived",   specs: ["responseHeaders"] },
+	{ name: "AuthRequired",      specs: ["responseHeaders"] },
+	{ name: "ResponseStarted",   specs: ["responseHeaders"] },
+	{ name: "BeforeRedirect",    specs: ["responseHeaders"] },
+	{ name: "Completed",         specs: ["responseHeaders"] }
+];
+
 function log (event, data)
 {
 	console.groupCollapsed(data.requestId, event);
@@ -5,85 +16,16 @@ function log (event, data)
 	console.groupEnd();
 }
 
-chrome.webRequest.onBeforeRequest.addListener(
-	(data)=>{
-		log("BeforeRequest", data);
-	},{
-		urls: ["<all_urls>"]
-	},[
-		"requestBody"
-	]
-);
-
-chrome.webRequest.onBeforeSendHeaders.addListener(
-	(data)=>{
-		log("BeforeSendHeaders", data);
-	},{
-		urls: ["<all_urls>"]
-	},[
-		"requestHeaders"
-	]
-);
-
-chrome.webRequest.onSendHeaders.addListener(
-	(data)=>{
-		log("SendHeaders", data);
-	},{
-		urls: ["<all_urls>"]
-	},[
-		"requestHeaders"
-	]
-);
-
-chrome.webRequest.onHeadersReceived.addListener(
-	(data)=>{
-		log("HeadersReceived", data);
-	},{
-		urls: ["<all_urls>"]
-	},[
-		"responseHeaders"
-	]
-);
-
-chrome.webRequest.onAuthRequired.addListener(
-	(data)=>{
-		log("AuthRequired", data);
-	},{
-		urls: ["<all_urls>"]
-	},[
-		"responseHeaders"
-	]
-);
-
-chrome.webRequest.onResponseStarted.addListener(
-	(data)=>{
-		log("ResponseStarted", data);
-	},{
-		urls: ["<all_urls>"]
-	},[
-		"responseHeaders"
-	]
-);
-
-chrome.webRequest.onBeforeRedirect.addListener(
-	(data)=>{
-		log("BeforeRedirect", data);
-	},{
-		urls: ["<all_urls>"]
-	},[
-		"responseHeaders"
-	]
-);
-
-chrome.webRequest.onCompleted.addListener(
-	(data)=>{
-		log("Completed", data);
-	},{
-		urls: ["<all_urls>"]
-	},[
-		"responseHeaders"
-	]
-);
+events.forEach((event)=>{
+	chrome.webRequest[`on${event.name}`].addListener(
+		(data)=>{
+			log(event.name, data);
+		},{
+			urls: ["<all_urls>"]
+		},
+		event.specs
+	);
+});
 
 chrome.webRequest.onErrorOccurred.addListener(
 	(data)=>{
