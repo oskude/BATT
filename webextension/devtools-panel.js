a2cd0fd945ce3db39328f7edeab127201be29ece
Andre Schmidt <andre.osku.schmidt@gmail.com>
2018-11-14 23:06:31 +0100

    initial custom log in devtools panel

--- webextension/devtools-panel.js
+++ webextension/devtools-panel.js
@@ -1,16 +1,54 @@
 var backendPort = browser.runtime.connect();
 var requests = {};
 
+Vue.component("request-event", {
+	props: ["data", "name"],
+	template: `
+		<span>{{ name }} </span>
+	`
+});
+
+Vue.component("request-entry", {
+	props: ["request", "id"],
+	template: `
+		<div>
+			<span>{{ id }}</span>
+			<span>{{ request.BeforeRequest.method }}</span>
+			<span>{{ request.BeforeRequest.type }}</span>
+			<span>{{ request.BeforeRequest.url }}</span>
+			<request-event
+				v-for="(data, name) in request"
+				v-bind:key="name"
+				v-bind:data="data"
+				v-bind:name="name"
+			></request-event>
+		</div>
+	`
+});
+
+var app = new Vue({
+	el: "#app",
+	data: {
+		requests: {}
+	},
+	template: `
+		<div>
+			<request-entry
+				v-for="(request, key) in requests"
+				v-bind:key="key"
+				v-bind:id="key"
+				v-bind:request="request"
+			></request-entry>
+		</div>
+	`
+});
+
 backendPort.onMessage.addListener(m=>{
 	if (m.type === "request/event") {
 		let id = m.data.requestId;
-		if (!requests[id]) {
-			requests[id] = {};
-			// TODO: this logs to background.js devtools! (not this panel devtools) bug or feature? (firefox 64.0b8)
-			console.groupCollapsed(id, m.data.method, m.data.url);
-			console.table(requests[id]);
-			console.groupEnd();
+		if (!app.requests[id]) {
+			Vue.set(app.requests, id, {});
 		}
-		requests[id][m.name] = m.data;
+		Vue.set(app.requests[id], m.name, m.data);
 	}
 });
